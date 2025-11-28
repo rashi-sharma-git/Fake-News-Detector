fc494bf67ac470dbd576b57575fb0948bc042ccf
Rajmaheshwari07 <168332722+Rajmaheshwari07@users.noreply.github.com>
2025-11-28 21:22:00 +0000

    raaj

--- truthguard-ai-main (1)/truthguard-ai-main/src/components/NavLink.tsx
+++ truthguard-ai-main (1)/truthguard-ai-main/src/components/NavLink.tsx
@@ -1,7 +1,7 @@
 import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
-
+//Raaj
 interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
   className?: string;
   activeClassName?: string;
